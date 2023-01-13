dc2b8d9e8cc202cb2c28498b3fe69be17e813785
Ivan <handlemind@gmail.com>
2023-01-13 23:26:26 +0100

    Module 2 solution

--- Module02-AsyncBasics/ThreadPoolExercises.Core/ThreadingHelpers.cs
+++ Module02-AsyncBasics/ThreadPoolExercises.Core/ThreadingHelpers.cs
@@ -7,24 +7,46 @@ namespace ThreadPoolExercises.Core
     {
         public static void ExecuteOnThread(Action action, int repeats, CancellationToken token = default, Action<Exception>? errorAction = null)
         {
-            // * Create a thread and execute there `action` given number of `repeats` - waiting for the execution!
-            //   HINT: you may use `Join` to wait until created Thread finishes
-            // * In a loop, check whether `token` is not cancelled
-            // * If an `action` throws and exception (or token has been cancelled) - `errorAction` should be invoked (if provided)
-
-
-
+            var t = new Thread(() =>
+            {
+                try
+                {
+                    for (int i = 0; i < repeats; i++)
+                    {
+                        token.ThrowIfCancellationRequested();
+                        action();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    errorAction?.Invoke(ex);
+                }
+            });
+            t.Start();
+            t.Join();
         }
 
         public static void ExecuteOnThreadPool(Action action, int repeats, CancellationToken token = default, Action<Exception>? errorAction = null)
         {
-            // * Queue work item to a thread pool that executes `action` given number of `repeats` - waiting for the execution!
-            //   HINT: you may use `AutoResetEvent` to wait until the queued work item finishes
-            // * In a loop, check whether `token` is not cancelled
-            // * If an `action` throws and exception (or token has been cancelled) - `errorAction` should be invoked (if provided)
+            var reset = new AutoResetEvent(false);
+            ThreadPool.QueueUserWorkItem((c) =>
+            {
+                try
+                {
+                    for (int i = 0; i < repeats; i++)
+                    {
+                        token.ThrowIfCancellationRequested();
+                        action();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    errorAction?.Invoke(ex);
+                }
 
-
-
+                reset.Set();
+            });
+            reset.WaitOne();
         }
     }
 }
