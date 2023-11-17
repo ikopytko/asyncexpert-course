e4cb684040275a2b51aeee39154fc3bb33035d5d
Ivan <handlemind@gmail.com>
2023-11-17 21:01:48 +0100

    Old changes

--- Module04-AsyncPart2/TaskCompletionSourceExercises.Core/AsyncTools.cs
+++ Module04-AsyncPart2/TaskCompletionSourceExercises.Core/AsyncTools.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace TaskCompletionSourceExercises.Core
@@ -8,7 +9,36 @@ namespace TaskCompletionSourceExercises.Core
     {
         public static Task<string> RunProgramAsync(string path, string args = "")
         {
-            return Task.FromResult(string.Empty);
+            var tcs = new TaskCompletionSource<string>();
+            var process = new Process();
+            process.EnableRaisingEvents = true;
+            process.StartInfo = new ProcessStartInfo(path, args)
+            {
+                RedirectStandardError = true,
+                RedirectStandardOutput = true
+            };
+            
+            process.Exited += (sender, eventArgs) =>
+            {
+                var senderProcess = sender as Process;
+
+                if (senderProcess.ExitCode != 0)
+                {
+                    var result = senderProcess?.StandardError.ReadToEnd();
+                    tcs.SetException(new Exception(result));
+                }
+                else
+                {
+                    var result = senderProcess?.StandardOutput.ReadToEnd();
+                    tcs.TrySetResult(result);
+                }
+                
+                senderProcess?.Dispose();
+            };
+
+            process.Start();
+
+            return tcs.Task;
         }
     }
 }
